b01a3edaeff44113f6055ed8301662bf8086721a
Santiago Cabrera <117690415+CabreraSantiago@users.noreply.github.com>
2022-11-17 22:55:29 -0500

    Update clase4_until.sh

--- TC4/clase4_until.sh
+++ TC4/clase4_until.sh
@@ -1,13 +1,13 @@
-## Ejercicio 1.10.3 Plant-Pollinator Networks ##
-# 1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y el número de columnas (plantas). #
-echo " 1. Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y el número de columnas (plantas). "
+# Ejercicio 10.3
+#  Pregunta 1
+echo "Escriba un script que tome uno de estos archivos y determine el número de filas (polinizadores) y el número de columnas (plantas). "
 cd ../../CSB-master/unix/data/Saavedra2013
 ls -v
 
 bash ../scriptconcatenado.sh n5.txt
 echo -e "\n"
-# 2. Escriba un script que imprima los números de filas y columnas para cada red. #
-echo " 2. Escriba un script que imprima los números de filas y columnas para cada red."
+# Pregunta 2
+echo "Escriba un script que imprima los números de filas y columnas para cada red."
 Files=*.txt
 
 for l in $Files
@@ -17,8 +17,8 @@ do
 	echo "Mis archivos:" $l "Filas:" $mifila "Columnas:" $micolumna
 done
 echo -e "\n"
-# 3. ¿Qué archivo tiene el mayor número de filas?¿Cuál tiene el mayor número de columnas?
-echo "3. ¿Qué archivo tiene el mayor número de filas?¿Cuál tiene el mayor número de columnas?"
+# Pregunta 3
+echo "¿Qué archivo tiene el mayor número de filas?¿Cuál tiene el mayor número de columnas?"
 echo "El mayor número de columnas"
 bash ../../scriptconcatenado2.sh | sort -n -r -k 3 | head -n 1
 echo -e "\n"
